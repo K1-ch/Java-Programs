490bd91f62e8ca95bb14730b923edb46f396a6d6
Kiran Chauhan <kc7563330@gmail.com>
2021-12-10 18:48:49 +0530

    Program to print Pattern:      *     **    ***   ****  *****
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ Pattern3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Pattern3{
+    public static void main(String [] args){
+     int m,i,j,k;
+     System.out.println("Enter the no. of lines");
+     Scanner sc=new Scanner(System.in);
+     m=sc.nextInt();
+     for(i=1;i<=m;i++){        
+        for(k=m;k>=i;k--){ 
+             System.out.print(" ");
+        }
+           for(j=1;j<=i;j++){   
+             System.out.print("*");
+         }
+       System.out.print("\n");
+     }
+
+    }
+}
