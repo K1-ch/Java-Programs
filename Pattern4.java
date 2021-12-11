4f15fc423fb8286aba58b3d8f2777f8dbb36416a
Kiran Chauhan <kc7563330@gmail.com>
2021-12-11 21:20:11 +0530

    Program to print pattern:  *********   *******    *****     ***      *
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ Pattern4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Pattern4{
+    public static void main(String [] args){
+    int n,i,j;
+    System.out.println("Enter the no. of lines");
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            System.out.print(" ");
+        }
+        for(j=1;j<=((2*n)-(2*i-1));j++){
+            System.out.print("*");
+        }
+         System.out.print("\n");
+    }}
+}
