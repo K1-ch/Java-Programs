9100d959e804f6a955534d08e51b467d78b3c13d
Kiran Chauhan <kc7563330@gmail.com>
2021-12-12 11:22:01 +0530

    Program to print pattern:
    * *  *   *    *     *      *       *        *         *          *           *
    
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pattern6{
+    public static void main(String [] args){
+        int n,i,j;
+        System.out.println("Enter the no. of lines");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+       for(i=0;i<n;i++){
+           for(j=0;j<i;j++){
+               System.out.print(" ");
+           }
+            System.out.print("*" );
+       }
+ System.out.print("\n" );
+    }
+}
