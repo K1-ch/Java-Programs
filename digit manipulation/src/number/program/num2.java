e13e755c498ed243ab5f49eb52db1e051318f1cd
Kiran Chauhan <kc7563330@gmail.com>
2021-12-31 19:24:07 +0530

    Program to  find a palindrome
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ digit manipulation/src/number/program/num2.java
@@ -0,0 +1,29 @@
+package number.program;
+import java.util.*;
+public class num2 {
+	public static void main(String[] args) {
+		int n,rem,rev=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int tmp=n;
+		while(1>0) {
+			while(n!=0) {
+				rem=n%10;
+				rev=rev*10+rem;
+				n=n/10;
+			}
+			if(rev!=tmp) {
+				n=tmp+rev;
+				tmp=n;
+				rev=0;	
+			}else {
+				System.out.println("Palindrome-->"+tmp);
+				break;
+			}
+		}
+		
+		
+	}
+	
+
+}
