cf1e1cf522b6541237227f6fc262621af23ebdd5
Kiran Chauhan <kc7563330@gmail.com>
2022-01-02 11:55:39 +0530

    count number of digits in a number Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ digit manipulation/src/number/program/Num5.java
@@ -0,0 +1,15 @@
+package number.program;
+import java.util.*;
+public class Num5 {
+public static void main(String[] args) {
+	int n,rem,count=0;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	while(n!=0) {
+		rem=n%10;
+		n=n/10;
+		count++;
+	}
+	System.out.println(count);
+        }
+}
