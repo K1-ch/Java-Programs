8418b2c28020abf3b3761d1ab8bf0c58f384ee11
Kiran Chauhan <kc7563330@gmail.com>
2022-01-05 23:44:15 +0530

    program to print a string and its length
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ StringManipulation/src/Stringprograms/com/String1.java
@@ -0,0 +1,11 @@
+package Stringprograms.com;
+import java.util.*;
+public class String1 {
+public static void main(String[] args) {
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	System.out.println(s);
+	System.out.println(s.length());
+}
+}
