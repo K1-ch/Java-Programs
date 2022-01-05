326354c560ed1ad8997cf475e81215f5043a159c
Kiran Chauhan <kc7563330@gmail.com>
2022-01-05 23:42:57 +0530

    program to print the pattern:
         *    *   *  * *

--- PatternPrograms/src/com/programs/pattern/Pattern6.java
+++ PatternPrograms/src/com/programs/pattern/Pattern6.java
@@ -6,7 +6,7 @@ class Pattern6{
         System.out.println("Enter the no. of lines");
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-       for(i=0;i<n;i++){
+       for(i=n;i>0;i--){
            for(j=0;j<i;j++){
                System.out.print(" ");
            }
