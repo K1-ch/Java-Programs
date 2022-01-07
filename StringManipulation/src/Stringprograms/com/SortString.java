185684850b41b84995b611504f4aa2d9e90fa1e6
Kiran Chauhan <kc7563330@gmail.com>
2022-01-07 22:05:00 +0530

    StringManipulation Zeroes to five Strong num Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ StringManipulation/src/Stringprograms/com/SortString.java
@@ -0,0 +1,26 @@
+package Stringprograms.com;
+import java.util.*;
+public class SortString {
+public static void main(String[] args) {
+	String s,newString="";
+	char tmp;
+	Scanner sc=new Scanner(System.in) ;
+	s=sc.nextLine();
+	char[] arr=s.toCharArray();
+	for(int i=0;i<arr.length;i++) {
+		for(int j=i+1;j<arr.length;j++) {
+		if((int)arr[i]>(int)arr[j]) {
+			tmp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=tmp;
+		}
+		}
+	}
+	
+    for(int i =0;i<arr.length;i++) {
+    	newString=newString+arr[i];
+    }
+		System.out.println(newString);
+	
+}
+}
