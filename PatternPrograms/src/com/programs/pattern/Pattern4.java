d36498f58396c4e0f0df63fa102de63cf13b680a
Kiran Chauhan <kc7563330@gmail.com>
2022-01-07 18:41:35 +0530

    reverse triangle
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

--- PatternPrograms/src/com/programs/pattern/Pattern4.java
+++ PatternPrograms/src/com/programs/pattern/Pattern4.java
@@ -8,9 +8,10 @@ class Pattern4{
     n=sc.nextInt();
     for(i=1;i<=n;i++){
         for(j=1;j<=i;j++){
-            System.out.print(" ");
+            System.out.print("  ");
         }
         for(j=1;j<=((2*n)-(2*i-1));j++){
+        	System.out.print(" ");
             System.out.print("*");
         }
          System.out.print("\n");
