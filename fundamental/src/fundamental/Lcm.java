00ecb065223161e56c5c227186b2532c15b26e7b
Kiran Chauhan <kc7563330@gmail.com>
2022-01-10 15:37:20 +0530

    to find lcm and hcf of two num
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ fundamental/src/fundamental/Lcm.java
@@ -0,0 +1,22 @@
+//to find lcm and hcf of two num
+package fundamental;
+import java.util.Scanner;
+public class Lcm {
+	public static void main(String[] args) {
+		int n,m,mul,gcd=1;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		m=sc.nextInt();
+		for(int i = 1; i <= n && i <= m; i++) {
+		      // Checks if i is factor of both integers
+		      if(n % i == 0 && m % i == 0)
+		        gcd = i;
+		  
+		    }
+
+		    int lcm = (n * m) / gcd;
+	System.out.println(lcm);
+	System.out.println(gcd);
+			}
+
+}
