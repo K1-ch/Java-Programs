06d0a3ca5608aa451368421912259dac4715206d
Kiran Chauhan <kc7563330@gmail.com>
2022-01-11 18:48:47 +0530

    Usage of switch in a program
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ fundamental/src/fundamental/UsingSwitch.java
@@ -0,0 +1,27 @@
+//usage o switches
+package fundamental;
+import java.util.Scanner;
+public class UsingSwitch {
+	public static void main(String[] args) {
+		 int n;
+		 final int  y=90;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		switch(n) {
+				case 1:
+		case 2:
+			System.out.println("Q 4");
+		case 3:
+			System.out.println("Q 4");
+			break;
+		case y:
+		case 5:
+		case 6:
+			System.out.println("Q 5");
+			default:
+				System.out.println("Q 4");
+				}
+		
+	     	}
+
+}
