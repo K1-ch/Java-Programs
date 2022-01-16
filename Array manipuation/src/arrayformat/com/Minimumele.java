83aff8a689049cd25ca518c3020870771244284c
Kiran Chauhan <kc7563330@gmail.com>
2022-01-16 22:05:21 +0530

    program to find minimum element in an array
    Signed-off-by: Kiran Chauhan <kc7563330@gmail.com>

---
+++ Array manipuation/src/arrayformat/com/Minimumele.java
@@ -0,0 +1,23 @@
+//minimum ele in  an array
+package arrayformat.com;
+import java.util.Scanner;
+public class Minimumele{
+ public static void main(String[] args) {
+	  int n,tmp;
+      Scanner sc=new Scanner(System.in);
+      n=sc.nextInt();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++){
+          a[i]=sc.nextInt();
+      }
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(a[i]>a[j]){
+                tmp=a[i];
+                a[i]=a[j];
+                a[j]=tmp;
+            }
+        }
+        System.out.println(n-1);
+	}             }
+}
