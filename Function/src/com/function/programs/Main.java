6c98c63ee61fbad7ccf0cf74dd75429aae7438e3
KIRAN CHAUHAN <kc7563330@gmail.com>
2022-03-14 22:39:58 +0530

    Program to add and subtract through methods
    Signed-off-by: KIRAN CHAUHAN <kc7563330@gmail.com>

---
+++ Function/src/com/function/programs/Main.java
@@ -0,0 +1,11 @@
+package com.function.programs;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sum s=new Sum();
+		System.out.println(s.sum(10, 20));
+        System.out.println(s.sub(90, 98));
+	}
+
+}
---
+++ Function/src/com/function/programs/Sum.java
@@ -0,0 +1,11 @@
+package com.function.programs;
+
+
+public class Sum{
+public int sum(int a, int b) {	
+	return a+b;
+}
+public int sub(int c,int d) {
+	return c-d;
+}
+}
