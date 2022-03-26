df78772b8883241d2ae9c46c4cdbba3321bcedea
KIRAN CHAUHAN <kc7563330@gmail.com>
2022-03-26 23:25:55 +0530

    Swap two values without third variables
    Signed-off-by: KIRAN CHAUHAN <kc7563330@gmail.com>

---
+++ Hi/src/com/javaprograms/Swap.java
@@ -0,0 +1,28 @@
+//Swap two values without third variables
+package com.javaprograms;
+
+import java.util.Scanner;
+
+class Solution {
+	public static void swapNumber(int x, int y) {
+		x = x + y;
+		y = x - y;
+		x = x - y;
+		int a[] = new int[2];
+		a[0] = x;
+		a[1] = y;
+		System.out.println("After swapping values are:");
+		System.out.print(a[0] + " " + a[1]);
+	}
+}
+
+public class Swap {
+
+	public static void main(String[] args) {
+		Solution p = new Solution();
+
+		p.swapNumber(20, -10);
+
+	}
+
+}
