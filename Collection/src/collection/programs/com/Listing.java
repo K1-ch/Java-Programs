cd501208657186de577d3cf0fc0272ec3078145b
KIRAN CHAUHAN <kc7563330@gmail.com>
2022-04-05 21:43:31 +0530

    //Program to use list ..
    Signed-off-by: KIRAN CHAUHAN <kc7563330@gmail.com>

---
+++ Collection/src/collection/programs/com/Listing.java
@@ -0,0 +1,17 @@
+//Program to use list ..
+package collection.programs.com;
+import java.util.ArrayList;
+import java.util.List;
+public class Listing {
+
+	public static void main(String[] args) {
+		List<Integer> a=new  ArrayList<>();
+		a.add(5);
+		a.add(9);
+		a.add(7);
+		a.add(67);
+		System.out.println(a);
+		
+	}
+
+}
