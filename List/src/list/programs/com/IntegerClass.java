fc8550956af71b629b95e3bf74ccd4a49fff4ae9
KIRAN CHAUHAN <kc7563330@gmail.com>
2022-04-08 18:26:55 +0530

    Adding and removing Elements in a String List
    Signed-off-by: KIRAN CHAUHAN <kc7563330@gmail.com>

---
+++ List/src/list/programs/com/IntegerClass.java
@@ -0,0 +1,25 @@
+package list.programs.com;
+//Adding and removing Elements in a String List
+import java.util.ArrayList;
+import java.util.List;
+
+class CharList{
+	void data() {
+	List <String> s=new ArrayList<>();
+	s.add("Kiran");//adding Strings in List
+	s.add("Pooja");
+	s.add("Laxmi");
+	s.add("Rahul");
+	s.remove(0);//remove an element in List
+		System.out.println(s);
+	s.forEach(System.out::println);
+		}
+}
+public class IntegerClass {
+
+	public static void main(String[] args) {
+	CharList s=new CharList();
+	s.data();
+	}
+
+}
