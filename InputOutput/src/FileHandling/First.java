4f0f7481fab062b35d08afa24ef6efdae7c7eb0e
KiranChauhan <kc7563330@gmail.com>
2022-05-27 07:34:57 +0530

    file handling first program
    Signed-off-by: KiranChauhan <kc7563330@gmail.com>

---
+++ InputOutput/src/FileHandling/First.java
@@ -0,0 +1,25 @@
+package FileHandling;
+
+//Importing input output classes
+import java.io.*;
+//Java Program to illustrate Reading from FileReader
+//using BufferedReader Class
+
+public class First {
+
+	public static void main(String[] args) throws IOException {
+
+		File file = new File("D://KIRAN//program.txt");
+
+		// Creating an object of BufferedReader class
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		// readLine() Reads line by line
+		String st;
+		// Condition holds true till
+		// there is character in a string
+		while ((st = br.readLine()) != null) {
+			System.out.println(st);
+		}
+	}
+
+}
