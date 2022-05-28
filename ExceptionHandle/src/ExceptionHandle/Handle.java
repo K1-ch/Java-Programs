ac70446c05f6ff36a0e2e302b525bebb41dccce4
KiranChauhan <kc7563330@gmail.com>
2022-05-29 01:55:03 +0530

    modify
    Signed-off-by: KiranChauhan <kc7563330@gmail.com>

--- ExceptionHandle/src/ExceptionHandle/Handle.java
+++ ExceptionHandle/src/ExceptionHandle/Handle.java
@@ -1,44 +1,45 @@
 package ExceptionHandle;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Exception{
+class ExceptionEx {
 	int a, b;
-	Exception(int a,int b){
-		this.a=a;
-		this.b=b;
-	}
+	Scanner sc = new Scanner(System.in);
+
+	
 	void exception() {
 		System.out.println("This is starting of Division");
+
 		try {
-		int res=a/b;
-		System.out.println("division result is="+res);
-		}
-		catch(ArithmeticException ae) {
-			
+			System.out.println("Enter values");
+			a = sc.nextInt();
+			b = sc.nextInt();
+			int res = a / b;
+			System.out.println("division result is=" + res);
+		} catch (ArithmeticException ae) {
+
 			System.out.println(ae.getMessage());
-			
+
 			System.out.println("Divide by Zero");
-		}
-		finally {
-			System.out.println("I am in Finally Block");
+		} catch (Throwable ae) {
+			System.out.println(ae.getMessage());
+			System.out.println("Enter any integer value");
+
+		} finally {
+			System.out.println("Always execute either exception or no exception");
+			sc.close();
 		}
 		System.out.println("This is ending of division function");
-		
-		
+
 	}
 }
+
 public class Handle {
 
 	public static void main(String[] args) {
-		int num1,num2;
-		Scanner sc=new Scanner(System.in);
-		num1=sc.nextInt();
-		num2=sc.nextInt();
-		Exception s=new Exception(num1,num2);
+		ExceptionEx s = new ExceptionEx();
 		s.exception();
-		sc.close();
-		
 
 	}
 
